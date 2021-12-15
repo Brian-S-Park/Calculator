c0427f3093a4ead1228b09802975c60778c07b44
Brian Park <brianshockerpark@gmail.com>
2021-12-14 22:11:47 -0800

    Update - 12/14/2021

--- script.js
+++ script.js
@@ -8,28 +8,38 @@ const Multiply_button=document.getElementById('multiply');
 const Plus_button=document.getElementById('plus');
 const Minus_button=document.getElementById('minus');
 const Sign_button=document.getElementById('sign');
-const Numbers=document.querySelectorAll('numbers');
-let buttonType="";
+const Numbers=document.querySelectorAll('[numbers]');
+const pastAnswers=document.getElementById('pastAnswers');
+const plus=document.getElementById('plus');
 
 //Numbers.forEach((button) =>
 //    button.addEventListener('click', () => console.log("meeeeep"))
 //)
 Equals_button.addEventListener('click',function(){
+    pastAnswers.innerHTML="30300 + 30303";
     Answer_div.innerHTML="4000000004";
     Clear_button.innerHTML="AC";
 });
 Clear_button.addEventListener('click',function(){
     Answer_div.innerHTML="0";
-    Clear_button.style.backgroundColor="grey";
-    greyAnimate();
+    pastAnswers.innerHTML="";
     Clear_button.innerHTML="C";
 });
 Sign_button.addEventListener('click',function(){
-    greyAnimate();
+    
 });
-function greyAnimate(){
-    setTimeout(() => {
-        Clear_button.style.backgroundColor="darkgrey";
-        Clear_button.style.transition="0.2s";
-    },200);
-}
+
+plus.addEventListener('click',function(){
+    
+});
+Numbers.forEach((button) => {
+    button.addEventListener('click', () => {
+      console.log("meeep");
+    });
+});
+// function greyAnimate(){
+//     setTimeout(() => {
+//         Clear_button.style.backgroundColor="darkgrey";
+//         Clear_button.style.transition="0.2s";
+//     },200);
+// }
