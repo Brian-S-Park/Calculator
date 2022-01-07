22f05e53d55fbb366b0765395b048b6fbb2bf0c8
Brian P <brianshockerpark@gmail.com>
2022-01-06 16:05:04 -0800

    Update - 01/06/2022

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
-//make list of operators in the eventlistener with if statements
-//make operator do math before displaying
 //get decimal function to work
 const Answer_div=document.getElementById('Answer');
 const Equals_button=document.getElementById('equals');
 const Clear_button=document.getElementById('AC');
 const Divide_button=document.getElementById('divide');
 const Multiply_button=document.getElementById('multiply');
-const Plus_button=document.getElementById('plus');
+//const Plus_button=document.getElementById('plus');
 const Minus_button=document.getElementById('minus');
 const Sign_button=document.getElementById('sign');
 const Numbers=document.getElementsByClassName('numbers');
 const zero=document.getElementsByClassName('longNumber');
 const pastAnswers=document.getElementById('pastAnswers');
 const plus=document.getElementById('+');
-const decimal=document.getElementsByClassName('bottomNumber');
+//const decimal=document.getElementsByClassName('bottomNumber');
 let operator=document.getElementsByClassName('orangeNumbers');
 let num=0;
+let next=0;
+let second=0;
+
+let count=0;
 
 Equals_button.addEventListener('click',function(){
     Clear_button.innerHTML="AC";
     num=reverseNumberFormat(getOutput());
     printOutput(num);
     pastAnswers.innerHTML=num;
-    console.log(pastAnswers.value);
+    //console.log(pastAnswers.value);
+    //alert(num);
 
 });
 Clear_button.addEventListener('click',function(){
     Answer_div.innerHTML="0";
+    for(let i=0;i<operator.length;i++){
+               operator[i].style.backgroundColor = "orangered";
+               operator[i].style.color = "white";
+               count=0;
+    }
+    if(Clear_button.innerHTML==="AC"){
+        printHistory("");
+        printOutput(0);
+    }
     if(Clear_button.innerHTML!="C"){
-        //alert("yooooo");
-        pastAnswers="";
+        if(pastAnswers===NaN){
+            printHistory("");
+        }
         Answer_div.innerHTML="0";
 
     }
     if(Clear_button.innerHTML==="C"){
-        //alert("yooooo");
         Clear_button.innerHTML="AC";
     }
 });
+
 Sign_button.addEventListener('click',function(){
     
 });
@@ -70,14 +83,27 @@ for(let i=0;i<operator.length;i++){
     operator[i].addEventListener('click',function(){
            //alert("This operator is clicked "+this.id);
            let output=reverseNumberFormat(getOutput());
-           if(output!=NaN){
-               //operator[i].
-
-               //Mske button change color and stay until math is doen
-               
-               //output=output+this.id;
-               //printOutput(output);
+           if(output!=0 && count<1){
+                count++;
+                operator[i].style.backgroundColor = "white";
+                operator[i].style.color = "orangered";
            }
+           //move next vairable and output in this area so that operation can occur in the if statements
+           if(this.id==="+"){
+               next=output;
+               console.log(next)
+
+               //printHistory(output);
+                Answer_div.innerHTML="";
+                // output=reverseNumberFormat(getOutput());
+                // console.log(output);
+                // num=next+output;
+                // console.log(num);
+               
+            }
+           if(this.id==='-'){printHistory(num);}
+           if(this.id==='*'){}
+           if(this.id==='/'){}
 
     });
 }
